878cb91bf8010fb7d926f726edbba5778ce9393d
lee10104 <lee95625@gmail.com>
2018-05-26 01:22:25 +0900

    changed and seperate django secret key

--- homepage/settings.py
+++ homepage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$+mxk7lh&twhxiy3c_3s%g-jjf7-grax5k4z!sn^&+zkw8*q31'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
